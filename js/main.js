0224d7ce396b707a3ee17dd3f759021bc2bb54a3
Sherbet <sherbetkabylbekova@gmail.com>
2021-06-30 17:39:57 +0600

    responsive

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-        items: 1,
+        items: 3,
         margin: 30,
         dots:true,
         loop: true,
         nav: true,
         navigationText: ['<div class="icon-arrow-left"></div>',
             '<div class="icon-arrow-right"></div>'],
-    });
+        responsive:{
+            320:{
+                items:1,
+            },
+            480:{
+                items:1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+
+});
+
 });
